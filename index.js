2564f6686931996208963878644efb112548c450
Syeda Umme Ayman <aymanshoity@gmail.com>
2023-10-21 07:24:41 +0600

    Product Addition to the cart Process is completed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app=express()
 const port=process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 // middleware
 app.use(cors())
@@ -29,11 +29,44 @@ async function run() {
     await client.connect();
 
     const productCollection=client.db('productDB').collection('products')
+    const storedProductCollection=client.db('storedProductDB').collection('storedProducts')
+
+    app.get('/products',async(req,res)=>{
+        const cursor=productCollection.find()
+        const result=await cursor.toArray()
+        res.send(result)
+    })
+    app.get('/products/:brand',async(req,res)=>{
+        const brand=req.params.brand
+        const query={brand: brand}
+        const cursor=productCollection.find(query)
+        const result=await cursor.toArray()
+        res.send(result)
+    })
+    app.get('/products/:brand/:id',async(req,res)=>{
+       const id=req.params.id
+        const query={_id: new ObjectId(id)}
+        const result=await productCollection.findOne(query)
+        res.send(result)
+    }) 
+    app.get('/storedProducts',async(req,res)=>{
+      const cursor=storedProductCollection.find()
+      const result=await cursor.toArray()
+      res.send(result)
+    }) 
+
 
     app.post('/products', async(req,res)=>{
         const newProduct=req.body;
         console.log(newProduct);
         const result=await productCollection.insertOne(newProduct);
+        res.send(result)
+
+    })
+    app.post('/storedProducts', async(req,res)=>{
+        const newProduct=req.body;
+        console.log(newProduct);
+        const result=await storedProductCollection.insertOne(newProduct);
         res.send(result)
 
     })
